be7d3b3fea94793639e3063e13230c95fc7e6cc4
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-10-12 18:10:49 +0200

    Make deployment work

--- backend/src/main/java/planit/people/preparation/PreparationApplication.java
+++ backend/src/main/java/planit/people/preparation/PreparationApplication.java
@@ -2,24 +2,11 @@ package planit.people.preparation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PreparationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PreparationApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/plan-it/**").allowedOrigins("http://localhost:3000", "http://localhost:80", "http://localhost:443");
-			}
-		};
 	}
 }
---
+++ backend/src/main/java/planit/people/preparation/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package planit.people.preparation;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:80", "http://localhost:443, https://planit-custom-domain.loca.lt"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
