2273c456e570544b63f0a1c981f52f290c98436e
Kaspr-whtev <marcin.kspr@gmail.com>
2022-10-13 09:08:20 +0200

    added a form to schedule a meeting, not tested due to the login/authorization bug

---
+++ frontend-react/src/components/CreateEventForm/AddEmail.tsx
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+
+
+type AddEmailProps = {
+    emails: string[];
+    addEmail: (email: string) => void;
+}
+
+export const AddEmail = ({emails, addEmail}: AddEmailProps) => {
+
+    const [email, setEmail] = useState('');
+
+    return (
+        <form>
+            <div>{emails.map((e, i) => <div key={i}>{e}</div>)}</div>
+            <input type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <button onClick={(e)=> {e.preventDefault()
+                addEmail(email)
+                setEmail('');
+            }}>+add email</button>
+        </form>
+    );
+}
---
+++ frontend-react/src/components/CreateEventForm/CreateEventForm.tsx
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from "react";
+
+import {AddEmail} from "./AddEmail";
+import {EventCreateRequest} from "../../models/event";
+
+
+type CreateEventFormProps = {
+    onSubmit: (result: EventCreateRequest) => void;
+}
+
+export const CreateEventForm = ({onSubmit}: CreateEventFormProps) => {
+    const [summary, setSummary] = useState('');
+    const [location, setLocation] = useState('');
+    const [description, setDescription] = useState('');
+    const [attendees, setAttendees] = useState<string[]>([]);
+    const [start_date, setStartDate] = useState('');
+    const [end_date, setEndDate] = useState('');
+    const [duration, setDuration] = useState(0);
+
+    const addEmail = (email:string) => {
+        setAttendees((prevState)=>[...prevState, email])
+    }
+
+    // const handleSubmit = (e : React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    //     e.preventDefault();
+    //     const result = {summary, location, description, attendees, start_date, end_date, duration};
+    //     console.log(result);
+    // }
+
+    //<input type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+    return (
+        <form>
+            <label htmlFor='summary'>Event Summary</label>
+            <input type='text' id='summary' placeholder='summary' value={summary} onChange={(e) => setSummary(e.target.value)}/>
+            <label htmlFor='location'>Event Location</label>
+            <input type='text' id='location' placeholder='location' value={location} onChange={(e) => setLocation(e.target.value)}/>
+            <label htmlFor='description'>Description</label>
+            <input type='text' id='description' placeholder='description' value={description} onChange={(e) => setDescription(e.target.value)}/>
+            <label htmlFor='attendees'>Attendees</label>
+            <AddEmail emails={attendees} addEmail={addEmail}></AddEmail>
+            <label htmlFor='start_date'>Start Date</label>
+            <input type='datetime-local' id='start_date' placeholder='start date' value={start_date} onChange={(e) => setStartDate(e.target.value)}/>
+            <label htmlFor='end_date'>End Date</label>
+            <input type='datetime-local' id='end_date' placeholder='end date' value={end_date} onChange={(e) => setEndDate(e.target.value)}/>
+            <label htmlFor='duration'>Event Duration</label>
+            <input type='number' id='duration' placeholder='duration' value={duration} onChange={(e) => setDuration(+e.target.value)}/>
+
+            <button onClick={(e) => {
+                e.preventDefault()
+                onSubmit({
+                summary,
+                location,
+                description,
+                attendees,
+                start_date,
+                end_date,
+                duration
+            })}}>submit</button>
+        </form>
+    );
+}
