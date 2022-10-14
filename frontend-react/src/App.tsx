9fa4660409070d31d7eb4ca4272eb238948b901e
Kaspr-whtev <marcin.kspr@gmail.com>
2022-10-14 11:46:11 +0200

    change from using url with id to sending the owner email in the request

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,58 +1,61 @@
-
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
-import { useGoogleLogin } from "@react-oauth/google";
+import {useGoogleLogin} from "@react-oauth/google";
 import axios from "axios";
 import {CreateEventForm} from "./components/CreateEventForm/CreateEventForm";
 import {EventCreateRequest} from "./models/event";
 
 function App() {
-  const onSuccess = (response: any) => {
-    console.log("succ: ", response);
-    axios
-      .post(
-        "/plan-it/user/token",
-        {
-          code: response.code,
-        },
-        {
-          headers: {
-            "Bypass-Tunnel-Reminder": 1,
-          },
-        }
-      )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => console.log(error.message));
-  };
+    const [owner, setOwner] = useState('');
+    const [accountEmails, setAccountEmails] = useState<string[]>([]);
 
-    const onEventSubmit = (result: EventCreateRequest) => {
-        console.log("succ: ", result);
+    const onSuccess = (response: any) => {
+        console.log("succ: ", response);
         axios
-            .post("/plan-it/calendar/new-event/1", result)
+            .post(
+                "/plan-it/user/token",
+                {
+                    code: response.code,
+                },
+                {
+                    headers: {
+                        "Bypass-Tunnel-Reminder": 1,
+                    },
+                }
+            )
             .then((response) => {
                 console.log(response);
             })
             .catch((error) => console.log(error.message));
     };
 
-  // @ts-ignore
-  const login = useGoogleLogin({
-    onSuccess: onSuccess,
-    flow: "auth-code",
-    scope:
-      "profile email openid https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly",
-    accessType: "offline",
-  });
+    const onEventSubmit = (result: EventCreateRequest) => {
+        console.log("succ: ", result);
+        axios
+            .post("/plan-it/calendar/new-event", result)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => console.log(error.message));
+    };
 
-  return (
-    <div className="App">
-      <button onClick={() => login()}>Sign in with Google 🚀 </button>;
-      <br/>
-      <CreateEventForm onSubmit={onEventSubmit}></CreateEventForm>
-    </div>
-  );
+    // @ts-ignore
+    const login = useGoogleLogin({
+        onSuccess: onSuccess,
+        flow: "auth-code",
+        scope:
+            "profile email openid https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly",
+        accessType: "offline",
+    });
+
+    return (
+        <div className="App">
+            <button onClick={() => login()}>Sign in with Google 🚀</button>
+            ;
+            <br/>
+            <CreateEventForm onSubmit={onEventSubmit} owner={owner}></CreateEventForm>
+        </div>
+    );
 }
 
 export default App;
