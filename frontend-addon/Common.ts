1583cc86a5af2f9b69b5c2c61a521b8f572195a4
JakubSeredynski <110531831+JakubSeredynski@users.noreply.github.com>
2022-10-24 20:41:13 +0200

    Update Common.ts

--- frontend-addon/Common.ts
+++ frontend-addon/Common.ts
@@ -1,11 +1,11 @@
-function onHomepage(e) {
+function onHomepage() {
   return createCard();
 }
 
 const weekInMs = 6.048e+8;
-const users = ["Kuba", "Marcin", "Łukasz", "Mustafa"]
+const constUsers = ["Kuba", "Marcin", "Łukasz", "Mustafa"]
 
-function createCard() {
+function createCard(listOfUsers : string[] = []) {
   var card = CardService.newCardBuilder();
   var header = CardService.newCardHeader().setTitle("Create new event");
 
@@ -45,20 +45,30 @@ function createCard() {
 
   section.addWidget(dropdown)
 
-
+  console.log("list passed to method " + listOfUsers)
+  card.addSection(buildUserSection(listOfUsers))
   card.addSection(section);
   card.setHeader(header);
   return card.build();
 }
 
-function buildUserSection(users : [string]) : GoogleAppsScript.Card_Service.CardSection
+function buildUserSection(users : string[]) : GoogleAppsScript.Card_Service.CardSection
 {
+  console.log("received by method " + users)
+
+  if(users === undefined || users === [])
+  {
+    console.log("users is undefined")
+    users = constUsers;
+  }
+
   const section = CardService.newCardSection()
       .setHeader("Guests")
       .setCollapsible(true)
 
   for (let i = 0; i < users.length; i++){
     const user = users[i];
+    console.log(user)
     section.addWidget(
         CardService.newTextParagraph().setText(user)
     )
@@ -71,11 +81,37 @@ function buildUserSection(users : [string]) : GoogleAppsScript.Card_Service.Card
           .setHint("user@gmail.com")
   )
 
+  var json = {}
+  json['users'] = JSON.stringify(users);
+
+  console.log(json)
   section.addWidget(
       CardService.newTextButton()
           .setText("add user")
-          .setOnClickAction()
-  )
+          .setOnClickAction(CardService.newAction()
+              .setFunctionName("onAddUser")
+              .setParameters(json)))
+  console.log("5");
+
+  console.log("at the end of method " + users)
 
   return section;
+}
+
+function onAddUser(e)
+{
+  const users = JSON.parse(e['parameters']['users']);
+
+  console.log("button received ");
+  console.log(e);
+  console.log("before add user " + users);
+
+  const newUser = e['formInputs']["add user"][0];
+  users.push(newUser)
+  e['listOfUsers'] = users;
+  console.log("after add users " + users);
+  const json = {};
+  json['listOfUsers'] = users;
+  var refreshNav = CardService.newNavigation().popToRoot().updateCard(createCard(users));
+  CardService.newActionResponseBuilder().setNavigation(refreshNav).build();
 }
