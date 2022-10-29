60e34d0241824281ed8a35b4c4d03a9228b1ab1e
Łukasz Blachnicki <6399265+Kartm@users.noreply.github.com>
2022-10-29 09:14:26 +0200

    Feature/20 Add linter, prettier, pre-push linter. Reformat all files. (#32)
    * Make TS more strict
    
    * Enable ESLint and Prettier
    
    * Format files according to new rules
    
    * Remove tsignore
    
    * Add husky
    
    * Finish configuration of linter
    
    * Configure linting before pushing

--- frontend-react/src/models/event.ts
+++ frontend-react/src/models/event.ts
@@ -1,10 +1,10 @@
 export type EventCreateRequest = {
-    summary: string,
-    location: string,
-    description: string,
-    attendee_emails: string[],
-    start_date: string,
-    end_date: string,
-    duration: Number,
-    owner_email: string
+  summary: string;
+  location: string;
+  description: string;
+  attendee_emails: string[];
+  start_date: string;
+  end_date: string;
+  duration: number;
+  owner_email: string;
 };
