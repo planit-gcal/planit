0df02d463df61e5fe73358bd313b381c58eedc32
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-11-07 08:10:50 +0100

    Add tabbed form

--- frontend-react/src/components/CreateEventForm/CreateEventForm.tsx
+++ frontend-react/src/components/CreateEventForm/CreateEventForm.tsx
@@ -1,4 +1,6 @@
+import { Form, Input, StepProps, Steps, Tabs, TabsProps } from 'antd';
 import { useState } from 'react';
+import { act } from 'react-dom/test-utils';
 
 import { EventCreateRequest } from '../../models/event';
 import { formatToUTC } from '../../utils/date.utils';
@@ -18,9 +20,81 @@ export const CreateEventForm = ({ onSubmit, owner }: CreateEventFormProps) => {
   const [end_date, setEndDate] = useState('');
   const [duration, setDuration] = useState(0);
 
+  const [activeTabKey, setActiveTabKey] = useState('0');
+
   const addEmail = (email: string) => {
     setAttendees((prevState) => [...prevState, email]);
   };
+
+  const stepItems = [
+    { title: 'General' },
+    { title: 'Google Event Details' },
+    { title: 'Search Details' },
+    { title: 'Availability' },
+    { title: 'Confirm' },
+  ];
+
+  const renderTabBar: TabsProps['renderTabBar'] = () => (
+    <Steps items={stepItems} current={+activeTabKey} onChange={(e) => setActiveTabKey(`${e}`)} />
+  );
+
+  const items = [
+    {
+      label: '',
+      key: '1',
+      children: (
+        <Form.Item name="event_name" label="Event Name" required>
+          <Input />
+        </Form.Item>
+      ),
+      forceRender: true,
+    },
+    {
+      label: '',
+      key: '2',
+      children: (
+        <Form.Item name="event_name" label="Description" required>
+          <Input />
+        </Form.Item>
+      ),
+      forceRender: true,
+    },
+    {
+      label: '',
+      key: '3',
+      children: (
+        <Form.Item name="event_name" label="Number of events" required>
+          <Input />
+        </Form.Item>
+      ),
+      forceRender: true,
+    },
+    {
+      label: '',
+      key: '4',
+      children: (
+        <Form.Item name="event_name" label="Exclude" required>
+          <Input />
+        </Form.Item>
+      ),
+      forceRender: true,
+    },
+    {
+      label: '',
+      key: '5',
+      children: (
+        <Form.Item name="event_name" label="Confirm" required>
+          <Input />
+        </Form.Item>
+      ),
+      forceRender: true,
+    },
+  ];
+  return (
+    <Form layout="vertical">
+      <Tabs activeKey={`${+activeTabKey + 1}`} items={items} renderTabBar={renderTabBar} />
+    </Form>
+  );
 
   return (
     <form>
