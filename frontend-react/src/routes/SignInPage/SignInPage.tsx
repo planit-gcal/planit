8d2d993df701a5a5037615008d5f107faa211e92
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-11-18 19:06:02 +0100

    Use types from library

--- frontend-react/src/routes/SignInPage/SignInPage.tsx
+++ frontend-react/src/routes/SignInPage/SignInPage.tsx
@@ -1,4 +1,4 @@
-//External imports
+import google from 'google-one-tap';
 import { useCallback, useContext, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -13,46 +13,31 @@ const GoogleAuth = () => {
   const { userDetails, setUserDetails } = useContext(PlanitUserContext);
   const navigate = useNavigate();
 
-  const googleButton = useRef(null);
+  const googleButton = useRef<HTMLDivElement>(null);
 
   const handleCredentialResponse = useCallback(
-    async (response: any) => {
+    async (response: google.CredentialResponse) => {
       const email = parseJwt(response.credential).email;
 
       const planitUserId = await getPlanItUserIdFromEmail(email);
 
       setUserDetails({ planitUserId, ownerEmail: email });
 
-      if (!planitUserId) {
-        console.log('navigate');
-
-        navigate('/sign-up');
-      } else {
-        navigate('/create-events');
-      }
+      navigate(!!planitUserId ? '/create-events' : '/sign-up');
     },
     [navigate, setUserDetails]
   );
 
   useEffect(() => {
-    const doStuff = async () => {
-      loadScript(src)
-        .then(async () => {
-          /*global google*/
-          // @ts-ignore
-          console.log(google);
-          // @ts-ignore
-          google.accounts.id.initialize({
-            client_id: googleOAuthClientId,
-            callback: handleCredentialResponse,
-          });
-          // @ts-ignore
-          google.accounts.id.renderButton(googleButton.current, { theme: 'outline', size: 'large' });
-        })
-        .catch(console.error);
-    };
-
-    doStuff();
+    loadScript(src)
+      .then(async () => {
+        google.accounts.id.initialize({
+          client_id: googleOAuthClientId,
+          callback: handleCredentialResponse,
+        });
+        google.accounts.id.renderButton(googleButton.current!, { theme: 'outline', size: 'large' });
+      })
+      .catch(console.error);
 
     return () => {
       const scriptTag = document.querySelector(`script[src="${src}"]`);
