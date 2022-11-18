99068ed64daf0629ab865fd50749db99f2a1ec8b
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-11-18 18:28:54 +0100

    Move utils to utils file

--- frontend-react/src/routes/SignInPage/SignInPage.tsx
+++ frontend-react/src/routes/SignInPage/SignInPage.tsx
@@ -5,37 +5,12 @@ import { useNavigate } from 'react-router-dom';
 import { getPlanItUserIdFromEmail } from '../../api/oauth/oauth.api';
 import { googleOAuthClientId } from '../../config';
 import { PlanitUserContext } from '../../contexts/PlanitUserContext';
+import { parseJwt, loadScript } from '../../utils/oauth.utils';
 
 const src = 'https://accounts.google.com/gsi/client';
 
-const loadScript = (src: any) =>
-  new Promise<void>((resolve, reject) => {
-    if (document.querySelector(`script[src="${src}"]`)) return resolve();
-    const script = document.createElement('script');
-    script.src = src;
-    script.onload = () => resolve();
-    script.onerror = (err) => reject(err);
-    document.body.appendChild(script);
-  });
-
-function parseJwt(token: any) {
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
-      .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join('')
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
 const GoogleAuth = () => {
-  const { isLoggedIn, userDetails, setUserDetails } = useContext(PlanitUserContext);
+  const { userDetails, setUserDetails } = useContext(PlanitUserContext);
   const navigate = useNavigate();
 
   const googleButton = useRef(null);
---
+++ frontend-react/src/utils/oauth.utils.ts
@@ -0,0 +1,25 @@
+export const loadScript = (src: string) =>
+  new Promise<void>((resolve, reject) => {
+    if (document.querySelector(`script[src="${src}"]`)) return resolve();
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = () => resolve();
+    script.onerror = (err) => reject(err);
+    document.body.appendChild(script);
+  });
+
+export const parseJwt = (token: string) => {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+};
