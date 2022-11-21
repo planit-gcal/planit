140fbcb40bf5869b9f51a184479db4d0f39061e1
Mustafa Alhamoud <61988029+mustafaMari@users.noreply.github.com>
2022-11-21 08:25:41 +0100

    Return null in case no user found with the provided email

--- backend/src/main/java/planit/people/preparation/APIs/API_OAuth.java
+++ backend/src/main/java/planit/people/preparation/APIs/API_OAuth.java
@@ -4,14 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import planit.people.preparation.DAOs.IDAO_GoogleAccount;
 import planit.people.preparation.Entities.Entity_GoogleAccount;
 import planit.people.preparation.Responses.UserCreationResponse;
 import planit.people.preparation.Scheduling.Converter;
 
 @RestController
-@RequestMapping(path = "plan-it/oauth", produces = { MediaType.APPLICATION_JSON_VALUE })
+@RequestMapping(path = "plan-it/oauth", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class API_OAuth {
 
     private final IDAO_GoogleAccount idaoGoogleAccount;
@@ -22,13 +25,14 @@ public class API_OAuth {
     }
 
     @GetMapping(path = "users")
-    public ResponseEntity<UserCreationResponse> getUserIdFromEmail(@RequestParam(name = "email")String email) {
+    public ResponseEntity<UserCreationResponse> getUserIdFromEmail(@RequestParam(name = "email") String email) {
         try {
-            Entity_GoogleAccount account = idaoGoogleAccount.getIdOfUserFromEmail(Converter.decodeURLString(email));
-            return new ResponseEntity<>(new UserCreationResponse(
-                    account.getThe_user().getUser_id(), account.getId()
-            ), HttpStatus.OK);
 
+            Entity_GoogleAccount account = idaoGoogleAccount.getIdOfUserFromEmail(Converter.decodeURLString(email));
+            UserCreationResponse response = account != null ?
+                    new UserCreationResponse(account.getThe_user().getUser_id(), account.getId()) :
+                    new UserCreationResponse(null, null);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
