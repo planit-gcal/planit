301ee86d7ab47e0b50d00c6bb79a0d401b3d3be0
Kaspr-whtev <marcin.kspr@gmail.com>
2022-12-06 15:05:53 +0100

    add sign up page

--- frontend-react/src/routes/SignInPage/SignUpPage.tsx
+++ frontend-react/src/routes/SignInPage/SignUpPage.tsx
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import { createNewUser } from '../../api/users/users.api';
 import { PlanitUserContext } from '../../contexts/PlanitUserContext';
 
+import {Typography, Col, Row, Button} from 'antd';
+
 export const SignUpPage = () => {
   const { isLoggedIn, setUserDetails } = useContext(PlanitUserContext);
   const navigate = useNavigate();
@@ -35,8 +37,18 @@ export const SignUpPage = () => {
   });
 
   return (
-    <div>
-      <button onClick={() => login()}>Sign up with Google 🚀</button>
-    </div>
+      <Row justify='center' gutter={16}>
+        <Col span={16}>
+          <Row justify='center'>
+            <Typography.Title level={2}>Welcome to PlanIt:</Typography.Title>
+          </Row>
+          <Row>
+            <Typography.Text>PlanIt is both a website and a Google Addon, it allows users to automatically schedule events for groups based on the desired criteria. After you sign in, you will fill out a form outlining what event you want to create, then the application will find the best time slot for your event.</Typography.Text>
+          </Row>
+          <Row justify='end'>
+            <Button type={'primary'} onClick={() => login()}>Sign up with Google</Button>
+          </Row>
+        </Col>
+      </Row>
   );
 };
