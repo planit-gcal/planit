43675a2dea8ab388fe915396c4864efd77017635
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-12-06 14:33:52 +0100

    Add possibility to remove your account

--- frontend-react/src/routes/ManageAccountPage/ManageAccountPage.tsx
+++ frontend-react/src/routes/ManageAccountPage/ManageAccountPage.tsx
@@ -1,3 +1,32 @@
+import { DeleteOutlined } from '@ant-design/icons';
+import { Button, notification } from 'antd';
+import { useCallback, useContext } from 'react';
+
+import { removeAccount } from '../../api/users/users.api';
+import { PlanitUserContext } from '../../contexts/PlanitUserContext';
+
 export const ManageAccountPage = () => {
-  return <div>manage account</div>;
+  const { userDetails, setUserDetails } = useContext(PlanitUserContext);
+
+  const onRemoveAccount = useCallback(async () => {
+    if (!userDetails?.planitUserId) {
+      return;
+    }
+
+    try {
+      await removeAccount(userDetails!.planitUserId!);
+      notification.success({ message: 'You have been successfully logged out!', placement: 'bottom' });
+      setUserDetails({ planitUserId: null, ownerEmail: null });
+    } catch (e) {
+      console.error(e);
+    }
+  }, [setUserDetails, userDetails]);
+
+  return (
+    <div>
+      <Button type="primary" danger onClick={() => onRemoveAccount()} icon={<DeleteOutlined />}>
+        Remove account
+      </Button>
+    </div>
+  );
 };
