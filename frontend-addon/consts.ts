8faf04e2adf528eac397e5b5e1b9bf8a9568bb4d
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-12-08 15:37:49 +0100

    Update temporary url

--- frontend-addon/consts.ts
+++ frontend-addon/consts.ts
@@ -1,62 +1,61 @@
-const defaultPreset: PresetDetails =
+const defaultPreset: PresetDetails = {
+  event_preset: {
+    name: "PlanIt Event",
+    break_into_smaller_events: false,
+  },
+  guests: [],
+  preset_availability: [
     {
-        "event_preset": {
-            "name": "PlanIt Event",
-            "break_into_smaller_events": false,
-        },
-        "guests": [],
-        "preset_availability": [
-            {
-                "day": "MONDAY",
-                "start_available_time": "08:00",
-                "end_available_time": "20:00",
-                "day_off": false
-            },
-            {
-                "day": "TUESDAY",
-                "start_available_time": "08:00",
-                "end_available_time": "20:00",
-                "day_off": false
-            },
-            {
-                "day": "WEDNESDAY",
-                "start_available_time": "08:00",
-                "end_available_time": "20:00",
-                "day_off": false
-            },
-            {
-                "day": "THURSDAY",
-                "start_available_time": "08:00",
-                "end_available_time": "20:00",
-                "day_off": false
-            },
-            {
-                "day": "FRIDAY",
-                "start_available_time": "08:00",
-                "end_available_time": "20:00",
-                "day_off": false
-            },
-            {
-                "day": "SATURDAY",
-                "start_available_time": "08:00",
-                "end_available_time": "20:00",
-                "day_off": false
-            },
-            {
-                "day": "SUNDAY",
-                "start_available_time": "08:00",
-                "end_available_time": "20:00",
-                "day_off": false
-            }
-        ]
-    }
+      day: "MONDAY",
+      start_available_time: "08:00",
+      end_available_time: "20:00",
+      day_off: false,
+    },
+    {
+      day: "TUESDAY",
+      start_available_time: "08:00",
+      end_available_time: "20:00",
+      day_off: false,
+    },
+    {
+      day: "WEDNESDAY",
+      start_available_time: "08:00",
+      end_available_time: "20:00",
+      day_off: false,
+    },
+    {
+      day: "THURSDAY",
+      start_available_time: "08:00",
+      end_available_time: "20:00",
+      day_off: false,
+    },
+    {
+      day: "FRIDAY",
+      start_available_time: "08:00",
+      end_available_time: "20:00",
+      day_off: false,
+    },
+    {
+      day: "SATURDAY",
+      start_available_time: "08:00",
+      end_available_time: "20:00",
+      day_off: false,
+    },
+    {
+      day: "SUNDAY",
+      start_available_time: "08:00",
+      end_available_time: "20:00",
+      day_off: false,
+    },
+  ],
+};
 
-const MAINURL = "https://planit-custom-domain.loca.lt"
+const MAINURL = "https://8d63-89-64-6-212.ngrok.io";
 // const MAINURL = "https://witty-bear-80.loca.lt"
 // const MAINURL = "http://localhost:8080"
-const weekInMs = 6.048e+8;
+const weekInMs = 6.048e8;
 const durationString = "duration";
-const eventNameString = "eventName"
+const eventNameString = "eventName";
 const usersString = "Users";
 const currentPresetIndexString = "currentPresetIndex";
 const minDateString = "startDate";
@@ -64,6 +63,6 @@ const maxDateString = "endDate";
 const errorString = "inputErrors";
 const addUserEmailString = "addUserEmail";
 const presetString = "Presets";
-const userTokenString = "userToken"
+const userTokenString = "userToken";
 const msSinceEpocToday = new Date();
 const iconUrl = "https://cdn-icons-png.flaticon.com/512/2569/2569174.png";
--- frontend-react/src/routes/SignInPage/SignInPage.tsx
+++ frontend-react/src/routes/SignInPage/SignInPage.tsx
@@ -1,3 +1,4 @@
+import { Typography, Col, Row } from 'antd';
 import { useCallback, useContext, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -5,8 +6,6 @@ import { getPlanItUserIdFromEmail } from '../../api/oauth/oauth.api';
 import { googleOAuthClientId } from '../../config';
 import { PlanitUserContext } from '../../contexts/PlanitUserContext';
 import { parseJwt, loadScript, clearScript } from '../../utils/oauth.utils';
-
-import {Typography, Col, Row} from 'antd';
 
 const src = 'https://accounts.google.com/gsi/client';
 
@@ -20,11 +19,15 @@ const GoogleAuth = () => {
     async (response: google.CredentialResponse) => {
       const email = parseJwt(response.credential).email;
 
-      const planitUserId = await getPlanItUserIdFromEmail(email);
+      try {
+        const planitUserId = await getPlanItUserIdFromEmail(email);
 
-      setUserDetails({ planitUserId, ownerEmail: email });
+        setUserDetails({ planitUserId, ownerEmail: email });
 
-      navigate(!!planitUserId ? '/create-events' : '/sign-up');
+        navigate(!!planitUserId ? '/create-events' : '/sign-up');
+      } catch (e) {
+        navigate('/sign-up');
+      }
     },
     [navigate, setUserDetails]
   );
@@ -47,12 +50,14 @@ const GoogleAuth = () => {
     return () => clearScript(src);
   }, [handleCredentialResponse, navigate, userDetails]);
 
-  return <Row style={{height:'100%'}} justify='center' align='middle'>
+  return (
+    <Row style={{ height: '100%' }} justify="center" align="middle">
       <Col>
-          <Typography.Title level={2}>Log in with Google:</Typography.Title>
-          <div className={"google-button"} ref={googleButton} />
+        <Typography.Title level={2}>Log in with Google:</Typography.Title>
+        <div className={'google-button'} ref={googleButton} />
       </Col>
-  </Row>;
+    </Row>
+  );
 };
 
 export default GoogleAuth;
